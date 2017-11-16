574b31245988a8017c6d4f7e06b30330966019ad
Michał Grochowski <mic.grochowski@gmail.com>
2017-11-16 10:47:15 +0100

    v0.2
    Started working on CV section

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,19 @@
 (function(){
     $(".nav__link--cv, .nav__link--contact").on("click", function(e){
-        e.preventDefault();
         $(".header").addClass("header--after-animation").removeClass("header--before-animation");
         $(".header__title").addClass("header__title--after-animation").removeClass("header__title--before-animation");
         $(".nav__link--back").addClass("nav__link--back--visible");
+        if (e.target && e.target.matches(".nav__link--cv")) {
+            $(".cv").addClass("section--visible");
+        } else if (e.target && e.target.matches(".nav__link--contact")) {
+            $(".contact").addClass("section--visible");
+        }
     });
 
     $(".nav__link--back").on("click", function(){
         $(".header").addClass("header--before-animation").removeClass("header--after-animation");
         $(".header__title").addClass("header__title--before-animation").removeClass("header__title--after-animation");
         $(".nav__link--back").removeClass("nav__link--back--visible");
+        $(".section").removeClass("section--visible");
     })
 })();
